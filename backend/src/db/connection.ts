670c00cbe6a6d9d0c26b822027fa17d29c3c61a1
Isaev Denis <sr_relikt@mail.ru>
2026-03-30 12:42:06 +0300

    fix: update path to db

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -3,13 +3,13 @@ import path from "path";
 import fs from "fs";
 
 import { logger } from "@/config/logger";
-
 import { runMigrations } from "./migrator";
 
 let db: Database.Database;
 
 export function initDB(): Database.Database {
-  const dbPath = path.join(__dirname, "../../../data/stats.db");
+  // Используем абсолютный путь внутри контейнера
+  const dbPath = path.join("/app", "data", "stats.db");
   const dir = path.dirname(dbPath);
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true });
